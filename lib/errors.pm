51e6899f35f4aa8462e0e72fe83418f7657aaed4
Ingy döt Net <ingy@ingy.net>
2014-07-30 15:41:35 -0700

    0.06

--- lib/errors.pm
+++ lib/errors.pm
@@ -25,7 +25,7 @@
 #------------------------------------------------------------------------------
 use strict; use strict;
 package errors;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub import {
     my ($class, $directive) = @_;
